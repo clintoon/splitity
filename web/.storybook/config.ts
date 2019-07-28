0888cfa12714007ba79b2cf4265ba807a3c86d10
clinton <clintonfeng@gmail.com>
2019-07-28 17:11:18 +1000

    Add storybook

---
+++ web/.storybook/config.ts
@@ -0,0 +1,8 @@
+import { configure } from '@storybook/react';
+
+const loadStories = () => {
+  const req = require.context('../src', true, /\.stories\.tsx$/);
+  req.keys().forEach(filename => req(filename));
+};
+
+configure(loadStories, module);
