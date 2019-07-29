323ff347dba04e5b4812ed105313413db13c23da
clinton <clintonfeng@gmail.com>
2019-07-29 23:38:31 +1000

    remove

--- web/storybook/config.ts
+++ web/storybook/config.ts
@@ -1,6 +1,5 @@
 import { configure, addDecorator } from '@storybook/react';
 import { withKnobs } from '@storybook/addon-knobs';
-import { withActions } from '@storybook/addon-actions';
 
 const loadStories = (): void => {
   const req = require.context('../src', true, /\.stories\.tsx$/);
