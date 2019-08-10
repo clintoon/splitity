5dc98fbd80465af889995c4c96ead2b0d5cad37c
clinton <clintonfeng@gmail.com>
2019-08-10 18:25:54 +1000

    basic routing and home page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,32 @@
 import React from 'react';
 import { GlobalStyle } from '@web/design/styles/GlobalStyle';
+import { Navbar } from '@web/design/components/Navbar/Navbar';
+import { Button, ButtonStyle } from '@web/design/components/Button/Button';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { HomePage } from '@web/pages/HomePage/HomePage';
+
+const renderPages = (): JSX.Element => {
+  return (
+    <div>
+      <Navbar
+        rightItems={[
+          <Button key="login" styleOf={ButtonStyle.Primary}>
+            Login
+          </Button>,
+        ]}
+      />
+      <BrowserRouter>
+        <Route exact path="/" component={HomePage} />
+      </BrowserRouter>
+    </div>
+  );
+};
 
 const App: React.FC<{}> = (): JSX.Element => {
   return (
     <React.Fragment>
       <GlobalStyle />
-      <div>Hello world!</div>
+      {renderPages()}
     </React.Fragment>
   );
 };
