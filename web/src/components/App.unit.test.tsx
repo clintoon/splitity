d008ca87e78f11e6ab5711d23d08c3cb6ca329ee
clinton <clintonfeng@gmail.com>
2019-08-11 00:52:30 +1000

    update App tests

--- web/src/components/App.unit.test.tsx
+++ web/src/components/App.unit.test.tsx
@@ -4,18 +4,18 @@ import { App } from '@web/components/App';
 import { GlobalStyle } from '@web/design/styles/GlobalStyle';
 import { PageContent } from '@web/components/PageContent';
 
-describe('<App/>', (): void => {
+describe('<App />', (): void => {
   let wrapper: ShallowWrapper;
 
   beforeEach((): void => {
     wrapper = shallow(<App />);
   });
 
-  it('renders GlobalStyle', (): void => {
+  it('renders <GlobalStyle />', (): void => {
     expect(wrapper.find(GlobalStyle).exists()).toBe(true);
   });
 
-  it('renders PageContent', (): void => {
+  it('renders <PageContent />', (): void => {
     expect(wrapper.find(PageContent).exists()).toBe(true);
   });
 });
