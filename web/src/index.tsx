c3b199e7622cdbe55478da3844162447f6a85a1c
Clinton <clintonfeng@gmail.com>
2019-08-12 23:46:30 +1000

    add redux and redux saga (#65)

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 import { App } from '@web/components/App';
+import { rootReducer } from '@web/reducers/rootReducer';
+
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
 
 const rootContainer = document.getElementById('root');
 
-ReactDOM.render(<App />, rootContainer);
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootContainer
+);
