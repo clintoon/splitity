7b974af460eab16bcd39de459bf20faf322e0234
clinton <clintonfeng@gmail.com>
2019-08-14 22:10:44 +1000

    add signIn

--- web/src/lib/auth/github/signIn.ts
+++ web/src/lib/auth/github/signIn.ts
@@ -2,11 +2,12 @@ import { auth } from 'firebase';
 import { firebaseApp } from '@web/lib/firebase';
 
 const provider = new auth.GithubAuthProvider();
-provider.addScope('repo');
+provider.addScope('user:email');
 
 const signIn = async (): Promise<void> => {
   try {
     const result = await firebaseApp.auth().signInWithRedirect(provider);
+    // If here, then we are already logged in.
     console.log('success: ', result);
   } catch (error) {
     console.log('fail: ', error);
