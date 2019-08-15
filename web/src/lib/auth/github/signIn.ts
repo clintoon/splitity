a20fddd201ad052f99d3ee3ca8f6c53b0373671c
clinton <clintonfeng@gmail.com>
2019-08-15 23:31:29 +1000

    fix signin

--- web/src/lib/auth/github/signIn.ts
+++ web/src/lib/auth/github/signIn.ts
@@ -4,14 +4,8 @@ import { firebaseApp } from '@web/lib/firebase';
 const provider = new auth.GithubAuthProvider();
 provider.addScope('user:email');
 
-const signIn = async (): Promise<void> => {
-  try {
-    const result = await firebaseApp.auth().signInWithRedirect(provider);
-    // If here, then we are already logged in.
-    console.log('success: ', result);
-  } catch (error) {
-    console.log('fail: ', error);
-  }
+const signIn = (): void => {
+  firebaseApp.auth().signInWithRedirect(provider);
 };
 
 export { signIn };
