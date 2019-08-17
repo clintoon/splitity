2b4eaac68637ff717019659dadf828a9e0c386eb
clinton <clintonfeng@gmail.com>
2019-08-17 16:47:54 +1000

    add test cov for navbar

--- web/src/components/Navbar.unit.test.tsx
+++ web/src/components/Navbar.unit.test.tsx
@@ -3,6 +3,9 @@ import { shallow, ShallowWrapper } from 'enzyme';
 import { NavbarForTest } from '@web/components/Navbar';
 import { Navbar as DesignNavbar } from '@web/design/components/Navbar/Navbar';
 import { History, createMemoryHistory } from 'history';
+import { FirebaseAuth } from '@web/lib/firebase/auth';
+
+jest.mock('@web/lib/firebase/auth');
 
 describe('<Navbar />', (): void => {
   let wrapper: ShallowWrapper;
@@ -15,5 +18,21 @@ describe('<Navbar />', (): void => {
 
   it('renders design <Navbar />', (): void => {
     expect(wrapper.find(DesignNavbar).exists()).toBe(true);
+  });
+
+  describe('signIn onClick', (): void => {
+    beforeEach((): void => {
+      const rightItems = wrapper.find(DesignNavbar).props()
+        .rightItems as JSX.Element[];
+
+      const loginButton = shallow(rightItems[0]);
+      loginButton.prop('onClick')();
+    });
+
+    it('calls auth redirectSignInWithGithub', (): void => {
+      expect(
+        FirebaseAuth.prototype.redirectSignInWithGithub
+      ).toHaveBeenCalled();
+    });
   });
 });
