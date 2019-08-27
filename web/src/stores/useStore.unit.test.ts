fa6d43ad4a0ad942c906ffe29c58728dfb1c5416
clinton <clintonfeng@gmail.com>
2019-08-27 22:11:56 +1000

    update useStore unit test

--- web/src/stores/useStore.unit.test.ts
+++
@@ -1,9 +0,0 @@
-import { useStore } from '@web/stores/useStore';
-
-describe('useStore', (): void => {
-  it('shows an exception when not wrapped in <StoreProvider />', (): void => {
-    expect((): void => {
-      useStore();
-    }).toThrow();
-  });
-});
