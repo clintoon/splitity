a879ef408be4ef5d79e1f3aef208c1b2220c3bc1
clinton <clintonfeng@gmail.com>
2019-08-27 23:57:53 +1000

    optionally can pass in store value to provider

--- web/src/stores/storeProvider.tsx
+++ web/src/stores/storeProvider.tsx
@@ -2,24 +2,34 @@ import React from 'react';
 import { useLocalStore } from 'mobx-react-lite';
 import { AuthModel, AuthModelType } from '@web/stores/authStore';
 
-export interface StoreProviderProps {
-  children: React.ReactNode;
-}
-
 export interface StoreType {
   auth: AuthModelType;
 }
 
+export interface StoreProviderProps {
+  children: React.ReactNode;
+  storeValues: Partial<StoreType>;
+}
+
 const storeContext = React.createContext<StoreType | null>(null);
 
-const StoreProvider = ({ children }: StoreProviderProps): JSX.Element => {
+const StoreProvider = ({
+  children,
+  storeValues,
+}: StoreProviderProps): JSX.Element => {
   const stores = {
-    auth: useLocalStore((): AuthModelType => AuthModel.create({})),
+    auth: useLocalStore(
+      (): AuthModelType => AuthModel.create(storeValues.auth || {})
+    ),
   };
 
   return (
     <storeContext.Provider value={stores}>{children}</storeContext.Provider>
   );
+};
+
+StoreProvider.defaultProps = {
+  storeValues: {},
 };
 
 export { StoreProvider, storeContext };
