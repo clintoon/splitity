a5acce65367689dc141c4c5ab3459a6479967794
clinton <clintonfeng@gmail.com>
2019-09-05 23:42:02 +1000

    migrate Button tests to rtl

--- web/src/design/components/Button/Button.unit.test.tsx
+++ web/src/design/components/Button/Button.unit.test.tsx
@@ -1,101 +1,52 @@
 import React from 'react';
-import { shallow, ShallowWrapper, mount, ReactWrapper } from 'enzyme';
-import {
-  Button,
-  ButtonSize,
-  ButtonStyle,
-  ButtonBaseForTest,
-} from '@web/design/components/Button/Button';
-import { Color } from '@web/design/styles/color';
+import { render, fireEvent, RenderResult } from '@testing-library/react';
+import { Button, ButtonStyle } from '@web/design/components/Button/Button';
+
+const BUTTON_LABEL = 'label';
+
+interface RenderButton {
+  styleOf: ButtonStyle;
+}
+
+interface RenderButtonResult {
+  renderResult: RenderResult;
+  onClickMock: jest.Mock;
+}
+
+const renderButton = ({ styleOf }: RenderButton): RenderButtonResult => {
+  const onClickMock = jest.fn();
+  const renderResult = render(
+    <Button styleOf={styleOf} onClick={onClickMock}>
+      {BUTTON_LABEL}
+    </Button>
+  );
+  return {
+    onClickMock,
+    renderResult,
+  };
+};
 
 describe('<Button/>', (): void => {
-  beforeEach((): void => {
-    jest.clearAllMocks();
+  it('renders the button label', (): void => {
+    const { renderResult } = renderButton({ styleOf: ButtonStyle.Primary });
+    expect(renderResult.getByRole('button')).toHaveTextContent(BUTTON_LABEL);
   });
 
-  describe('basic', (): void => {
-    let wrapper: ShallowWrapper;
-    const onClickMock = jest.fn();
-
-    beforeEach((): void => {
-      wrapper = shallow(
-        <Button styleOf={ButtonStyle.Primary} onClick={onClickMock}>
-          label
-        </Button>
-      );
+  it('calls onClick prop when button is clicked', (): void => {
+    const { renderResult, onClickMock } = renderButton({
+      styleOf: ButtonStyle.Primary,
     });
 
-    it('renders the button label', (): void => {
-      expect(wrapper.find(ButtonBaseForTest).text()).toBe('label');
-    });
+    fireEvent.click(renderResult.getByRole('button'));
 
-    it('calls onClick prop when button is clicked', (): void => {
-      wrapper.find(ButtonBaseForTest).simulate('click');
-      expect(onClickMock).toHaveBeenCalledTimes(1);
-    });
+    expect(onClickMock).toHaveBeenCalled();
   });
 
-  describe('styleOf prop', (): void => {
-    const testStyleOfPropColor = (
-      testValues: { styleOf: ButtonStyle; backgroundColor: string }[]
-    ): void => {
-      testValues.forEach(({ styleOf, backgroundColor }): void => {
-        describe(`${styleOf} color`, (): void => {
-          let wrapper: ReactWrapper;
+  it('does not call onClick prop when not clicked', (): void => {
+    const { onClickMock } = renderButton({
+      styleOf: ButtonStyle.Primary,
+    });
 
-          beforeEach((): void => {
-            wrapper = mount(<Button styleOf={styleOf}>label</Button>);
-          });
-
-          it(`applies ${styleOf} style`, (): void => {
-            expect(wrapper.find(ButtonBaseForTest)).toHaveStyleRule(
-              'background-color',
-              backgroundColor
-            );
-          });
-        });
-      });
-    };
-
-    testStyleOfPropColor([
-      { styleOf: ButtonStyle.Primary, backgroundColor: Color.Orange },
-      {
-        styleOf: ButtonStyle.Secondary,
-        backgroundColor: Color.LightGray,
-      },
-    ]);
-  });
-
-  describe('size prop', (): void => {
-    const testSizePropPadding = (
-      testValues: { size: ButtonSize; padding: string }[]
-    ): void => {
-      testValues.forEach(({ size, padding }): void => {
-        describe(`${size} size`, (): void => {
-          let wrapper: ReactWrapper;
-
-          beforeEach((): void => {
-            wrapper = mount(
-              <Button styleOf={ButtonStyle.Primary} size={size}>
-                label
-              </Button>
-            );
-          });
-
-          it(`has correct padding`, (): void => {
-            expect(wrapper.find(ButtonBaseForTest)).toHaveStyleRule(
-              'padding',
-              padding
-            );
-          });
-        });
-      });
-    };
-
-    testSizePropPadding([
-      { size: ButtonSize.Small, padding: '6px 10px' },
-      { size: ButtonSize.Medium, padding: '12px 18px' },
-      { size: ButtonSize.Large, padding: '18px 26px' },
-    ]);
+    expect(onClickMock).not.toHaveBeenCalled();
   });
 });
