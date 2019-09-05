7ebb7828dbebb179bfc567ce92ea42396e30f3f5
Clinton <clintonfeng@gmail.com>
2019-09-05 23:54:14 +1000

    migrate Button tests to rtl (#126)

--- web/setupTest.ts
+++ web/setupTest.ts
@@ -1,4 +1,8 @@
 import 'jest-styled-components';
+import '@testing-library/jest-dom/extend-expect';
+import { toMatchImageSnapshot } from 'jest-image-snapshot';
+
+expect.extend({ toMatchImageSnapshot });
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
