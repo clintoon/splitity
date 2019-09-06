05c247fdd22a032c64dd97e38c84fad6f0336946
Clinton <clintonfeng@gmail.com>
2019-09-06 22:04:16 +1000

    migrate to text component tests to rtl (#129)
    * migrate to rtl

--- web/src/design/components/Text/Text.unit.test.tsx
+++ web/src/design/components/Text/Text.unit.test.tsx
@@ -1,75 +1,174 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import { render, RenderResult } from '@testing-library/react';
 import {
   Text,
-  TextAs,
+  TEXT_TEST_ID,
   TextStyle,
-  TextProps,
+  TextAs,
   TextAlign,
 } from '@web/design/components/Text/Text';
 
-type OptionalTextProps = Omit<Partial<TextProps>, 'children'>;
-type TestProp = 'styleOf' | 'as' | 'textAlign' | 'margin';
+const TEXT_CHILD = 'I am some words';
+
+interface RenderTextOptions {
+  styleOf?: TextStyle;
+  as?: TextAs;
+  margin?: string;
+  textAlign?: TextAlign;
+}
+
+const renderText = (options: RenderTextOptions): RenderResult => {
+  return render(<Text {...options}>{TEXT_CHILD}</Text>);
+};
 
 describe('<Text/>', (): void => {
-  const testStyleOfTextSnapshots = (
-    testName: string,
-    testProp: TestProp,
-    props: OptionalTextProps[]
-  ): void => {
-    props.forEach((prop): void => {
-      it(`${testName} ${prop[testProp]}`, (): void => {
-        const wrapper = mount(<Text {...prop}>example</Text>);
-        expect(wrapper.getDOMNode()).toMatchSnapshot();
+  describe('test html tag', (): void => {
+    it('displays correct html tag for default props', (): void => {
+      const { getByTestId } = renderText({});
+      expect(getByTestId(TEXT_TEST_ID)).toContainHTML('div');
+    });
+
+    it('displays correct html tag for when as prop is div', (): void => {
+      const { getByTestId } = renderText({ as: TextAs.Div });
+      expect(getByTestId(TEXT_TEST_ID)).toContainHTML('div');
+    });
+
+    it('displays correct html tag for when as prop is h1', (): void => {
+      const { getByTestId } = renderText({ as: TextAs.H1 });
+      expect(getByTestId(TEXT_TEST_ID)).toContainHTML('h1');
+    });
+
+    it('displays correct html tag for when as prop is h2', (): void => {
+      const { getByTestId } = renderText({ as: TextAs.H2 });
+      expect(getByTestId(TEXT_TEST_ID)).toContainHTML('h2');
+    });
+
+    it('displays correct html tag for when as prop is h3', (): void => {
+      const { getByTestId } = renderText({ as: TextAs.H3 });
+      expect(getByTestId(TEXT_TEST_ID)).toContainHTML('h3');
+    });
+
+    it('displays correct html tag for when as prop is h4', (): void => {
+      const { getByTestId } = renderText({ as: TextAs.H4 });
+      expect(getByTestId(TEXT_TEST_ID)).toContainHTML('h4');
+    });
+
+    it('displays correct html tag for when as prop is h5', (): void => {
+      const { getByTestId } = renderText({ as: TextAs.H5 });
+      expect(getByTestId(TEXT_TEST_ID)).toContainHTML('h5');
+    });
+
+    it('displays correct html tag for when as prop is h6', (): void => {
+      const { getByTestId } = renderText({ as: TextAs.H6 });
+      expect(getByTestId(TEXT_TEST_ID)).toContainHTML('h6');
+    });
+
+    it('displays correct html tag for when as prop is p', (): void => {
+      const { getByTestId } = renderText({ as: TextAs.P });
+      expect(getByTestId(TEXT_TEST_ID)).toContainHTML('p');
+    });
+  });
+
+  describe('test rendering of text', (): void => {
+    describe('as prop', (): void => {
+      it('displays text when as prop is h1', (): void => {
+        const { getByTestId } = renderText({ as: TextAs.H1 });
+        expect(getByTestId(TEXT_TEST_ID)).toHaveTextContent(TEXT_CHILD);
+      });
+
+      it('displays text when as prop is h2', (): void => {
+        const { getByTestId } = renderText({ as: TextAs.H2 });
+        expect(getByTestId(TEXT_TEST_ID)).toHaveTextContent(TEXT_CHILD);
+      });
+
+      it('displays text when as prop is h3', (): void => {
+        const { getByTestId } = renderText({ as: TextAs.H3 });
+        expect(getByTestId(TEXT_TEST_ID)).toHaveTextContent(TEXT_CHILD);
+      });
+
+      it('displays text when as prop is h4', (): void => {
+        const { getByTestId } = renderText({ as: TextAs.H4 });
+        expect(getByTestId(TEXT_TEST_ID)).toHaveTextContent(TEXT_CHILD);
+      });
+
+      it('displays text when as prop is h5', (): void => {
+        const { getByTestId } = renderText({ as: TextAs.H5 });
+        expect(getByTestId(TEXT_TEST_ID)).toHaveTextContent(TEXT_CHILD);
+      });
+
+      it('displays text when as prop is h6', (): void => {
+        const { getByTestId } = renderText({ as: TextAs.H6 });
+        expect(getByTestId(TEXT_TEST_ID)).toHaveTextContent(TEXT_CHILD);
+      });
+
+      it('displays text when as prop is div', (): void => {
+        const { getByTestId } = renderText({ as: TextAs.Div });
+        expect(getByTestId(TEXT_TEST_ID)).toHaveTextContent(TEXT_CHILD);
+      });
+
+      it('displays text when as prop is p', (): void => {
+        const { getByTestId } = renderText({ as: TextAs.P });
+        expect(getByTestId(TEXT_TEST_ID)).toHaveTextContent(TEXT_CHILD);
       });
     });
-  };
 
-  it('has correct default html tag and css properties', (): void => {
-    const wrapper = mount(<Text>example</Text>);
-    expect(wrapper.getDOMNode()).toMatchSnapshot();
-  });
+    describe('styleOf prop', (): void => {
+      it('displays text when styleOf prop is body', (): void => {
+        const { getByTestId } = renderText({ styleOf: TextStyle.Body });
+        expect(getByTestId(TEXT_TEST_ID)).toHaveTextContent(TEXT_CHILD);
+      });
 
-  describe('styleOf prop', (): void => {
-    testStyleOfTextSnapshots(
-      'has correct CSS properties for',
-      'styleOf',
-      Object.values(TextStyle).map(
-        (propValue): OptionalTextProps => {
-          return { styleOf: propValue };
-        }
-      )
-    );
-  });
+      it('displays text when styleOf prop is title1', (): void => {
+        const { getByTestId } = renderText({ styleOf: TextStyle.Title1 });
+        expect(getByTestId(TEXT_TEST_ID)).toHaveTextContent(TEXT_CHILD);
+      });
 
-  describe('as prop', (): void => {
-    testStyleOfTextSnapshots(
-      'has correct html tag for',
-      'as',
-      Object.values(TextAs).map(
-        (propValue): OptionalTextProps => {
-          return { as: propValue };
-        }
-      )
-    );
-  });
+      it('displays text when styleOf prop is title2', (): void => {
+        const { getByTestId } = renderText({ styleOf: TextStyle.Title2 });
+        expect(getByTestId(TEXT_TEST_ID)).toHaveTextContent(TEXT_CHILD);
+      });
 
-  describe('textAlign prop', (): void => {
-    testStyleOfTextSnapshots(
-      'has correct CSS properties for',
-      'textAlign',
-      Object.values(TextAlign).map(
-        (propValue): OptionalTextProps => {
-          return { textAlign: propValue };
-        }
-      )
-    );
-  });
+      it('displays text when styleOf prop is title3', (): void => {
+        const { getByTestId } = renderText({ styleOf: TextStyle.Title3 });
+        expect(getByTestId(TEXT_TEST_ID)).toHaveTextContent(TEXT_CHILD);
+      });
 
-  describe('margin prop', (): void => {
-    it('has correct CSS properties when set', (): void => {
-      const wrapper = mount(<Text margin="1 1 1 1">example</Text>);
-      expect(wrapper.getDOMNode()).toMatchSnapshot();
+      it('displays text when styleOf prop is title4', (): void => {
+        const { getByTestId } = renderText({ styleOf: TextStyle.Title4 });
+        expect(getByTestId(TEXT_TEST_ID)).toHaveTextContent(TEXT_CHILD);
+      });
+
+      it('displays text when styleOf prop is title5', (): void => {
+        const { getByTestId } = renderText({ styleOf: TextStyle.Title5 });
+        expect(getByTestId(TEXT_TEST_ID)).toHaveTextContent(TEXT_CHILD);
+      });
+
+      it('displays text when styleOf prop is title6', (): void => {
+        const { getByTestId } = renderText({ styleOf: TextStyle.Title6 });
+        expect(getByTestId(TEXT_TEST_ID)).toHaveTextContent(TEXT_CHILD);
+      });
+    });
+
+    it('displays text when there is margin', (): void => {
+      const { getByTestId } = renderText({ margin: '4 4 4 4' });
+      expect(getByTestId(TEXT_TEST_ID)).toHaveTextContent(TEXT_CHILD);
+    });
+
+    describe('textAlign', (): void => {
+      it('displays text when text align is center', (): void => {
+        const { getByTestId } = renderText({ textAlign: TextAlign.Center });
+        expect(getByTestId(TEXT_TEST_ID)).toHaveTextContent(TEXT_CHILD);
+      });
+
+      it('displays text when text align is right', (): void => {
+        const { getByTestId } = renderText({ textAlign: TextAlign.Right });
+        expect(getByTestId(TEXT_TEST_ID)).toHaveTextContent(TEXT_CHILD);
+      });
+
+      it('displays text when text align is left', (): void => {
+        const { getByTestId } = renderText({ textAlign: TextAlign.Left });
+        expect(getByTestId(TEXT_TEST_ID)).toHaveTextContent(TEXT_CHILD);
+      });
     });
   });
 });
