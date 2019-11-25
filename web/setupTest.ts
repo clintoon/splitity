3318d7f9ef17d7a8101671acb4e0000fa8c8b4bf
clinton <clintonfeng@gmail.com>
2019-11-25 21:14:15 +1100

    openPage tests

--- web/setupTest.ts
+++ web/setupTest.ts
@@ -1,1 +1,7 @@
 import '@testing-library/jest-dom/extend-expect';
+import { GITHUB_APP_NAME } from '@web/testing/testGlobals';
+
+// Setup globals
+process.env = Object.assign(process.env, {
+  GITHUB_APP_NAME: GITHUB_APP_NAME,
+});
---
+++ web/src/testing/testGlobals.ts
@@ -0,0 +1,3 @@
+const GITHUB_APP_NAME = 'split-my-prs-test';
+
+export { GITHUB_APP_NAME };
