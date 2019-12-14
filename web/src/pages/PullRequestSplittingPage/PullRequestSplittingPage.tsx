c20dd5dc195ebe8f1101879034278f9647a91935
clinton <clintonfeng@gmail.com>
2019-12-14 11:57:03 +1100

    Add fetching of PR diff

--- web/src/pages/PullRequestSplittingPage/PullRequestSplittingPage.tsx
+++ web/src/pages/PullRequestSplittingPage/PullRequestSplittingPage.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { match } from 'react-router-dom';
 import { GithubAPI } from '@web/lib/github/github';
 import { PullRequestInfoPage } from '@web/pages/PullRequestSplittingPage/PullRequestInfo';
+import parseDiff from 'parse-diff';
 
 interface MatchProps {
   owner: string;
@@ -38,11 +39,37 @@ const useGetPRTitle = (
   return title;
 };
 
+const useGetPRDiff = (
+  owner: string,
+  repoName: string,
+  pullRequestId: number
+): parseDiff.File[] | undefined => {
+  const [PRDiff, setPRDiff] = useState<parseDiff.File[]>();
+  useEffect((): void => {
+    const callback = async (): Promise<void> => {
+      const githubApi = new GithubAPI();
+      const fileDiffs = await githubApi.getPullRequestDiff({
+        owner,
+        repoName,
+        pullRequestId,
+      });
+
+      setPRDiff(fileDiffs);
+    };
+    callback();
+  }, []);
+
+  return PRDiff;
+};
+
 const PullRequestSplittingPage = ({
   match,
 }: PullRequestSplittingPageProps): JSX.Element => {
   const { owner, repoName, pullRequestId } = match.params;
   const title = useGetPRTitle(owner, repoName, Number(pullRequestId));
+  const PRDiff = useGetPRDiff(owner, repoName, Number(pullRequestId));
+
+  console.log('PRDiff', PRDiff);
 
   return (
     <PullRequestInfoPage
