2bb5a4ad66fab03b707cb3102f42295e23f4797f
clinton <clintonfeng@gmail.com>
2019-12-14 11:56:09 +1100

    Adds colors & font for code

--- web/src/design/styles/color.ts
+++ web/src/design/styles/color.ts
@@ -6,6 +6,12 @@ enum Color {
   DarkGray = '#6D6767',
   Black = '#000000',
   White = '#FFFFFF',
+
+  LineNumberGreen = '#85c280',
+  LightGreen = '#9EE498',
+  LineNumberRed = '#FAA3A3',
+  LightRed = '#FAA3A3',
+  LineNumberGray = '#FFFFFF',
 }
 
 export { Color };
