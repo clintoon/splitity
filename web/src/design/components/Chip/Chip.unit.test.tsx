686830567fabbd5becb95679ab013f0da004be05
clinton <clintonfeng@gmail.com>
2019-12-17 20:11:03 +1100

    add display chip label unit test

--- web/src/design/components/Chip/Chip.unit.test.tsx
+++ web/src/design/components/Chip/Chip.unit.test.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
-import { render, RenderResult, fireEvent } from '@testing-library/react';
+import {
+  render,
+  RenderResult,
+  fireEvent,
+  within,
+} from '@testing-library/react';
 import { Chip, CHIP_TESTID } from '@web/design/components/Chip/Chip';
+
+const LABEL = 'label123';
 
 interface RenderChipOptions {
   onClick?: () => void;
@@ -13,19 +20,24 @@ interface RenderChipResult {
 
 const renderChip = (options: RenderChipOptions): RenderChipResult => {
   const renderResult = render(
-    <Chip onClick={options.onClick} onDelete={options.onDelete} label="hello" />
+    <Chip onClick={options.onClick} onDelete={options.onDelete} label={LABEL} />
   );
 
   return { renderResult };
 };
 
 describe('<Chip/>', (): void => {
+  it('displays label', (): void => {
+    const { renderResult } = renderChip({});
+    const chipContainer = renderResult.getByTestId(CHIP_TESTID);
+    expect(within(chipContainer).queryByText(LABEL)).not.toBe(null);
+  });
+
   it('calls onClick when clicking the chip', (): void => {
     const onClickMock = jest.fn();
 
     const { renderResult } = renderChip({
       onClick: onClickMock,
-      onDelete: undefined,
     });
 
     fireEvent.click(renderResult.getByTestId(CHIP_TESTID));
