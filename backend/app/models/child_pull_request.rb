fd24505b9035d7decaf427ded6dc373682e399ca
clinton <clintonfeng@gmail.com>
2020-01-05 20:58:38 +1100

    create child pr model and update split job column to parent_pr_id

---
+++ backend/app/models/child_pull_request.rb
@@ -0,0 +1,2 @@
+class ChildPullRequest < ApplicationRecord
+end
--- backend/app/models/split_pull_request_job.rb
+++ backend/app/models/split_pull_request_job.rb
@@ -2,7 +2,7 @@
 class SplitPullRequestJob < ApplicationRecord
   enum status: { queued: 0, success: 1, failed: 2 }
 
-  validates :splitted_pr_id, presence: true
+  validates :parent_pr_id, presence: true
   validates :split_initiated_by_user_id, presence: true
   validates :repo_id, presence: true
 end
