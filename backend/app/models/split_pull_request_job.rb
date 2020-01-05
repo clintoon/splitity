14c9974537348a7366bf4ce6073dbf630a0802c1
clinton <clintonfeng@gmail.com>
2020-01-05 13:50:54 +1100

    add validations and unit tests

--- backend/app/models/split_pull_request_job.rb
+++ backend/app/models/split_pull_request_job.rb
@@ -1,4 +1,8 @@
 # Model for the info for split pull request jobs
 class SplitPullRequestJob < ApplicationRecord
   enum status: { pending: 0, in_progress: 1, success: 2, failed: 3 }
+
+  validates :parent_pull_request_id, presence: true
+  validates :split_initiated_by_user_id, presence: true
+  validates :repo_owner_user_id, presence: true
 end
--- backend/test/models/split_pull_request_job_test.rb
+++ backend/test/models/split_pull_request_job_test.rb
@@ -1,7 +1,39 @@
 require 'test_helper'
 
 class SplitPullRequestJobTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'valid SplitPullRequestJob' do
+    split_pr_job = SplitPullRequestJob.new(\
+      parent_pull_request_id: 1,\
+      split_initiated_by_user_id: 1,\
+      repo_owner_user_id: 1
+    )
+    assert split_pr_job.valid?, ''
+  end
+
+  test 'invalid SplitPullRequestJob without parent_pull_request_id value' do
+    split_pr_job = SplitPullRequestJob.new(\
+      split_initiated_by_user_id: 1,\
+      repo_owner_user_id: 1
+    )
+    assert split_pr_job.invalid?
+    assert_not_nil split_pr_job.errors[:parent_pull_request_id]
+  end
+
+  test 'invalid SplitPullRequestJob without split_initiated_by_user_id value' do
+    split_pr_job = SplitPullRequestJob.new(\
+      parent_pull_request_id: 1,\
+      repo_owner_user_id: 1
+    )
+    assert split_pr_job.invalid?
+    assert_not_nil split_pr_job.errors[:split_initiated_by_user_id]
+  end
+
+  test 'invalid SplitPullRequestJob without repo_owner_user_id value' do
+    split_pr_job = SplitPullRequestJob.new(\
+      parent_pull_request_id: 1,\
+      split_initiated_by_user_id: 1\
+    )
+    assert split_pr_job.invalid?
+    assert_not_nil split_pr_job.errors[:repo_owner_user_id]
+  end
 end
