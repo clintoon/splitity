eb4d3c5798a693c50bd9bd10fa23ab27583a0822
clinton <clintonfeng@gmail.com>
2020-01-05 21:24:56 +1100

    use correct belongs_to key

--- backend/app/models/child_pull_request.rb
+++ backend/app/models/child_pull_request.rb
@@ -1,4 +1,4 @@
 # Model for the child PRs created by splitting the parent PR
 class ChildPullRequest < ApplicationRecord
-  belongs_to :split_pull_request_jobs
+  belongs_to :split_pull_request_job
 end
