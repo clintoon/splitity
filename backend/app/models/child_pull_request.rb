08fd2c1d9d487fd107b9eadd86edbf8a05af2fa2
clinton <clintonfeng@gmail.com>
2020-01-05 21:20:52 +1100

    add associations

--- backend/app/models/child_pull_request.rb
+++ backend/app/models/child_pull_request.rb
@@ -1,2 +1,4 @@
+# Model for the child PRs created by splitting the parent PR
 class ChildPullRequest < ApplicationRecord
+  belongs_to :split_pull_request_jobs
 end
--- backend/app/models/split_pull_request_job.rb
+++ backend/app/models/split_pull_request_job.rb
@@ -2,6 +2,8 @@
 class SplitPullRequestJob < ApplicationRecord
   enum status: { queued: 0, success: 1, failed: 2 }
 
+  has_many :child_pull_requests, dependent: :destroy
+
   validates :parent_pr_id, presence: true
   validates :split_initiated_by_user_id, presence: true
   validates :repo_id, presence: true
