0998d38cef0beb17e6a4882c3076a985486c0da9
clinton <clintonfeng@gmail.com>
2020-01-06 19:29:17 +1100

    refactor split pr job test to use factory bot

--- backend/test/models/split_pull_request_job_test.rb
+++ backend/test/models/split_pull_request_job_test.rb
@@ -2,94 +2,63 @@ require 'test_helper'
 
 class SplitPullRequestJobTest < ActiveSupport::TestCase
   test 'valid SplitPullRequestJob with status queued' do
-    split_pr_job = SplitPullRequestJob.new(\
-      parent_pr_id: 1,\
-      split_initiated_by_user_id: 1,\
-      repo_id: 'abc123'\
-    )
+    split_pr_job = build(:split_pull_request_job)
 
     assert split_pr_job.valid?
   end
 
   test 'invalid SplitPullRequestJob without parent_pr_id value' do
-    split_pr_job = SplitPullRequestJob.new(\
-      split_initiated_by_user_id: 1,\
-      repo_id: 'abc123'\
-    )
+    split_pr_job = build(:split_pull_request_job)
+    split_pr_job.parent_pr_id = nil
 
     assert split_pr_job.invalid?
     assert_not_nil split_pr_job.errors[:parent_pr_id]
   end
 
   test 'invalid SplitPullRequestJob without split_initiated_by_user_id value' do
-    split_pr_job = SplitPullRequestJob.new(\
-      parent_pr_id: 1,\
-      repo_id: 'abc123'\
-    )
+    split_pr_job = build(:split_pull_request_job)
+    split_pr_job.split_initiated_by_user_id = nil
 
     assert split_pr_job.invalid?
     assert_not_nil split_pr_job.errors[:split_initiated_by_user_id]
   end
 
   test 'invalid SplitPullRequestJob without repo_id value' do
-    split_pr_job = SplitPullRequestJob.new(\
-      parent_pr_id: 1,\
-      split_initiated_by_user_id: 1\
-    )
+    split_pr_job = build(:split_pull_request_job)
+    split_pr_job.repo_id = nil
 
     assert split_pr_job.invalid?
     assert_not_nil split_pr_job.errors[:repo_id]
   end
 
   test 'SplitPullRequestJob by default is queued' do
-    split_pr_job = SplitPullRequestJob.new(\
-      parent_pr_id: 1,\
-      split_initiated_by_user_id: 1,\
-      repo_id: 'abc123'\
-    )
+    split_pr_job = build(:split_pull_request_job)
 
     assert split_pr_job.queued?
   end
 
   test 'SplitPullRequestJob can set state to success' do
-    split_pr_job = SplitPullRequestJob.new(\
-      parent_pr_id: 1,\
-      split_initiated_by_user_id: 1,\
-      repo_id: 'abc123'\
-    )
-
+    split_pr_job = build(:split_pull_request_job)
     split_pr_job.status = 'success'
+
     assert split_pr_job.success?
     assert_not split_pr_job.queued?
     assert split_pr_job.valid?
   end
 
   test 'SplitPullRequestJob can set state to failed' do
-    split_pr_job = SplitPullRequestJob.new(\
-      parent_pr_id: 1,\
-      split_initiated_by_user_id: 1,\
-      repo_id: 'abc123'\
-    )
-
+    split_pr_job = build(:split_pull_request_job)
     split_pr_job.status = 'failed'
+
     assert split_pr_job.failed?
     assert_not split_pr_job.queued?
     assert split_pr_job.valid?
   end
 
   test 'SplitPullRequestJob can have multiple child pull requests' do
-    split_pr_job = SplitPullRequestJob.new(\
-      id: 1, \
-      parent_pr_id: 1,\
-      split_initiated_by_user_id: 1,\
-      repo_id: 'abc123'\
-    )
-
-    split_pr_job.save
-
-    child_pr1 = ChildPullRequest.new(child_pr_id: 1)
-    child_pr2 = ChildPullRequest.new(child_pr_id: 2)
-
+    split_pr_job = create(:split_pull_request_job)
+    child_pr1 = build(:child_pull_request)
+    child_pr2 = build(:child_pull_request)
     split_pr_job.child_pull_requests << [child_pr1, child_pr2]
 
     result = SplitPullRequestJob.last.child_pull_requests
@@ -97,18 +66,9 @@ class SplitPullRequestJobTest < ActiveSupport::TestCase
   end
 
   test 'SplitPullRequestJob destroys associated child pull requests when destroyed' do
-    split_pr_job = SplitPullRequestJob.new(\
-      id: 1, \
-      parent_pr_id: 1,\
-      split_initiated_by_user_id: 1,\
-      repo_id: 'abc123'\
-    )
-
-    split_pr_job.save
-
-    child_pr1 = ChildPullRequest.new(child_pr_id: 1)
-    child_pr2 = ChildPullRequest.new(child_pr_id: 2)
-
+    split_pr_job = create(:split_pull_request_job)
+    child_pr1 = build(:child_pull_request)
+    child_pr2 = build(:child_pull_request)
     split_pr_job.child_pull_requests << [child_pr1, child_pr2]
 
     split_pr_job.destroy
