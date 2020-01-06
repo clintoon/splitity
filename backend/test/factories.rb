1e170244357ba1d8b1dd3dbba08e011cff8c00fb
clinton <clintonfeng@gmail.com>
2020-01-06 19:29:01 +1100

    add factory bot

---
+++ backend/test/factories.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+  factory :split_pull_request_job do
+    parent_pr_id { 1 }
+    split_initiated_by_user_id { 1 }
+    repo_id { 'repo123' }
+  end
+
+  factory :child_pull_request do
+    child_pr_id { 1 }
+    split_pull_request_job
+  end
+end
