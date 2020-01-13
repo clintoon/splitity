f8f3c67f8f91bf9b68378c55d8c79db755c06f3f
clinton <clintonfeng@gmail.com>
2020-01-13 22:59:31 +1100

    fix unit tests by adding mocks

--- backend/test/controllers/pull_requests_controller_test.rb
+++ backend/test/controllers/pull_requests_controller_test.rb
@@ -26,17 +26,25 @@ class PullRequestsControllerTest < ActionDispatch::IntegrationTest
   test 'splitting PR returns an 200 OK when Access-Token header is valid' do
     valid_access_token = 'abc123'
 
-    mock = Minitest::Mock.new
-    mock.expect :current_user, id: 1
-    mock.expect :repository, { id: 1 }, [{ name: 'test01', owner: 'clintoon' }]
+    github_mock = Minitest::Mock.new
+    github_mock.expect :current_user, id: 1, login: 'clintoon'
+    github_mock.expect :repository, { id: 1 }, [{ name: 'test01', owner: 'clintoon' }]
+    github_mock.expect :permission_level, { permission: 'write' }, [{ name: 'test01', owner: 'clintoon' }, 'clintoon']
+    github_mock.expect :repository, { id: 1 }, [{ name: 'test01', owner: 'clintoon' }]
 
-    GithubService.stub(:new, mock) do
-      post '/v1/repos/clintoon/test01/pulls/123/split',\
-           params: { patches: ['example patch'] },\
-           headers: { 'HTTP_ACCESS_TOKEN': valid_access_token }
+    github_app_mock = Minitest::Mock.new
+    github_app_mock.expect :repo_installation_id, 'installation_id123', [{ name: 'test01', owner: 'clintoon' }]
+
+    GithubAppService.stub(:new, github_app_mock) do
+      GithubService.stub(:new, github_mock) do
+        post '/v1/repos/clintoon/test01/pulls/123/split',\
+             params: { patches: ['example patch'] },\
+             headers: { 'HTTP_ACCESS_TOKEN': valid_access_token }
+      end
     end
 
     assert_response :success
-    assert_mock mock
+    assert_mock github_mock
+    assert_mock github_app_mock
   end
 end
