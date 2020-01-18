092c7f372a5d2d21bfabc817a4e990196c31ace5
Clinton Feng <clintonfeng@gmail.com>
2020-01-18 11:39:59 +1100

    use sidekiq (#551)
    * add sidekiq
    
    * use default if cannot get redis url

---
+++ backend/config/initializers/sidekiq.rb
@@ -0,0 +1,14 @@
+if !Rails.application.credentials.redis.nil?
+  REDIS_URL = Rails.application.credentials.redis[:uri]
+
+  Sidekiq.configure_server do |config|
+    config.redis = { url: REDIS_URI }
+  end
+
+  Sidekiq.configure_client do |config|
+    config.redis = { url: REDIS_URI }
+  end
+
+else
+  Rails.logger.warn('Unable to find redis url in credentials. Using defaults')
+end
