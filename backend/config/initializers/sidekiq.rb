830207059bc06b623a38765fca0999a17f0d08a2
clinton <clintonfeng@gmail.com>
2020-01-18 11:30:34 +1100

    use default if cannot get redis url

--- backend/config/initializers/sidekiq.rb
+++ backend/config/initializers/sidekiq.rb
@@ -1,4 +1,4 @@
-unless ENV['RAILS_ENV'] == 'test'
+if !Rails.application.credentials.redis.nil?
   REDIS_URL = Rails.application.credentials.redis[:uri]
 
   Sidekiq.configure_server do |config|
@@ -8,4 +8,7 @@ unless ENV['RAILS_ENV'] == 'test'
   Sidekiq.configure_client do |config|
     config.redis = { url: REDIS_URI }
   end
+
+else
+  Rails.logger.warn('Unable to find redis url in credentials. Using defaults')
 end
