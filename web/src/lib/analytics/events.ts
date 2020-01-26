fb58718f505ab9a08dc107e48eedeb2cffa266dd
clinton <clintonfeng@gmail.com>
2020-01-26 23:58:15 +1100

    open page events

--- web/src/lib/analytics/events.ts
+++ web/src/lib/analytics/events.ts
@@ -1,6 +1,8 @@
 enum TrackingEvent {
   signUpCompleted = 'sign_up_completed',
   onRedirectToAuthScreen = 'on_redirect_to_auth_screen',
+  openConfigureAppInstallationPage = 'open_app_installation_page',
+  openInstallAppPage = 'open_install_app_page',
 }
 
 export { TrackingEvent };
