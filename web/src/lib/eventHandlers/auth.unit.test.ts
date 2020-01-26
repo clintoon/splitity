9aea922fae24fb901007a1860a2ab1654cde81b9
clinton <clintonfeng@gmail.com>
2020-01-26 23:51:10 +1100

    fix test

--- web/src/lib/eventHandlers/auth.unit.test.ts
+++ web/src/lib/eventHandlers/auth.unit.test.ts
@@ -2,6 +2,7 @@ import { FirebaseAuth } from '@web/lib/firebase/auth';
 import { handleSignIn } from '@web/lib/eventHandlers/auth';
 
 jest.mock('@web/lib/firebase/auth');
+jest.mock('@web/lib/analytics/tracking');
 
 describe('handleSignin', (): void => {
   it('calls redirectSignInWithGithub', (): void => {
