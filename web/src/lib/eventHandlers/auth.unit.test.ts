2608870c87d332cd4ecd4583db9c2cc02dd921dc
clinton <clintonfeng@gmail.com>
2020-01-27 22:34:15 +1100

    onRedirectToAuthScreen track test

--- web/src/lib/eventHandlers/auth.unit.test.ts
+++ web/src/lib/eventHandlers/auth.unit.test.ts
@@ -1,5 +1,7 @@
 import { FirebaseAuth } from '@web/lib/firebase/auth';
 import { handleSignIn } from '@web/lib/eventHandlers/auth';
+import { track } from '@web/lib/analytics/tracking';
+import { TrackingEvent } from '../analytics/events';
 
 jest.mock('@web/lib/firebase/auth');
 jest.mock('@web/lib/analytics/tracking');
@@ -8,5 +10,10 @@ describe('handleSignin', (): void => {
   it('calls redirectSignInWithGithub', (): void => {
     handleSignIn();
     expect(FirebaseAuth.prototype.redirectSignInWithGithub).toHaveBeenCalled();
+  });
+
+  it('calls onRedirectToAuthScreen tracking event', (): void => {
+    handleSignIn();
+    expect(track).toHaveBeenCalledWith(TrackingEvent.onRedirectToAuthScreen);
   });
 });
