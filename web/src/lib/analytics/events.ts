f4de148782bf75077a97534393dd652d5a902926
clinton <clintonfeng@gmail.com>
2020-01-27 14:33:24 +1100

    add events defs

--- web/src/lib/analytics/events.ts
+++ web/src/lib/analytics/events.ts
@@ -1,8 +1,10 @@
 enum TrackingEvent {
   signUpCompleted = 'sign_up_completed',
   onRedirectToAuthScreen = 'on_redirect_to_auth_screen',
-  openConfigureAppInstallationPage = 'open_app_installation_page',
+  openConfigureAppInstallationPage = 'open_configure_app_installation_page',
   openInstallAppPage = 'open_install_app_page',
+  loadSplitPRPage = 'load_split_pull_request_page',
+  onSplitPRSuccess = 'on_split_pull_request_success',
 }
 
 export { TrackingEvent };
