5100309f50de51262d5a5ba27fa90ef85b515549
clinton <clintonfeng@gmail.com>
2020-02-07 23:38:15 +1100

    update webpack config

--- web/webpack.dev.js
+++ web/webpack.dev.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = merge(common, {
   mode: 'development',
--- web/webpack.prod.js
+++ web/webpack.prod.js
@@ -3,4 +3,15 @@ const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'production',
+  plugins: [
+    new webpack.EnvironmentPlugin([
+      'REACT_APP_FIREBASE_KEY',
+      'REACT_APP_FIREBASE_DOMAIN',
+      'REACT_APP_FIREBASE_PROJECT_ID',
+      'REACT_APP_FIREBASE_APP_ID',
+      'GITHUB_APP_NAME',
+      'BACKEND_API_URL',
+      'MIXPANEL_TOKEN',
+    ]),
+  ],
 });
