4f666121b3f8aff3863d3a052d54df5e5c0f2071
clinton <clintonfeng@gmail.com>
2020-02-08 14:51:00 +1100

    update tags

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -6,7 +6,7 @@ provider "digitalocean" {
 }
 
 resource "digitalocean_kubernetes_cluster" "splitity-k8s-cluster" {
-  name    = "${local.environment}-web-k8s-cluster"
+  name    = "splitity-${local.environment}-web-k8s-cluster"
   region  = "nyc1"
   version = "1.16.6-do.0"
 
@@ -18,26 +18,26 @@ resource "digitalocean_kubernetes_cluster" "splitity-k8s-cluster" {
     max_nodes  = 5
   }
 
-  tags = ["splitity", "${local.environment}"]
+  tags = ["splitity-${local.environment}"]
 }
 
 resource "digitalocean_database_cluster" "postgres-cluster" {
-  name       = "${local.environment}-postgres-cluster"
+  name       = "splitity-${local.environment}-postgres-cluster"
   engine     = "pg"
   version    = "12"
   size       = "db-s-1vcpu-1gb"
   region     = "nyc1"
   node_count = 1
 
-  tags = ["splitity", "${local.environment}"]
+  tags = ["splitity-${local.environment}"]
 }
 
 resource "digitalocean_database_cluster" "redis-cluster" {
-  name       = "${local.environment}-redis-cluster"
+  name       = "splitity-${local.environment}-redis-cluster"
   engine     = "redis"
   size       = "db-s-1vcpu-1gb"
   region     = "nyc1"
   node_count = 1
 
-  tags = ["splitity", "${local.environment}"]
+  tags = ["splitity-${local.environment}"]
 }
