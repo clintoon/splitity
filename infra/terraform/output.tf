896d3e22894acc79077bf0962a903f7d7d1bb659
clinton <clintonfeng@gmail.com>
2020-02-12 20:48:52 +1100

    delete output terraform

--- infra/terraform/output.tf
+++
@@ -1,9 +0,0 @@
-output "splitity_redis_cluster_password" {
-  value = "${digitalocean_database_cluster.splitity-postgres-cluster.password}",
-  sensitive   = true
-}
-
-output "splitity_redis_cluster_password" {
-  value = "${digitalocean_database_cluster.splitity-redis-cluster.password}",
-  sensitive   = true
-}
