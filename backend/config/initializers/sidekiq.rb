e1921449eb89af69e33f3c8c33abcb73c2a12084
clinton <clintonfeng@gmail.com>
2020-02-12 22:02:40 +1100

    setup sidekiq password

--- backend/config/initializers/sidekiq.rb
+++ backend/config/initializers/sidekiq.rb
@@ -1,12 +1,13 @@
 if !Rails.application.credentials.redis.nil?
   REDIS_URI = Rails.application.credentials.redis[:uri]
+  REDIS_PASSWORD = Rails.application.credentials.redis[:password]
 
   Sidekiq.configure_server do |config|
-    config.redis = { url: REDIS_URI }
+    config.redis = { url: REDIS_URI, password: REDIS_PASSWORD }
   end
 
   Sidekiq.configure_client do |config|
-    config.redis = { url: REDIS_URI }
+    config.redis = { url: REDIS_URI, password: REDIS_PASSWORD }
   end
 
 else
