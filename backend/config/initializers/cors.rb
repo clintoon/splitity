48412e00384bf4dc2c8533475959740d7696f991
clinton <clintonfeng@gmail.com>
2020-02-16 21:16:29 +1100

    change cors allowed origins to be splitity.com in prod

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:8080'
+    origins Rails.application.credentials.cors[:allowed_origins]
 
     resource '*',
              headers: :any,
