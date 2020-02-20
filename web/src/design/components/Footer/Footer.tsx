9080c08da0bb998254fc96310156069a81c01e07
clinton <clintonfeng@gmail.com>
2020-02-21 00:15:19 +1100

    make footer css mobile responsive

--- web/src/design/components/Footer/Footer.tsx
+++ web/src/design/components/Footer/Footer.tsx
@@ -3,15 +3,7 @@ import styled from 'styled-components';
 import { Text } from '@web/design/components/Text/Text';
 import { Color } from '@web/design/styles/color';
 import { Link } from '@web/design/components/Link/Link';
-
-const Container = styled.div`
-  display: flex;
-  height: 40px;
-  background-color: ${Color.Gray200};
-  justify-content: center;
-  align-items: center;
-  flex-shrink: 0;
-`;
+import { Breakpoint } from '@web/design/styles/mediaQuery';
 
 interface Link {
   id: string;
@@ -24,23 +16,52 @@ interface FooterProps {
   links: Link[];
 }
 
+const Container = styled.div`
+  display: flex;
+  height: 40px;
+  background-color: ${Color.Gray200};
+  justify-content: center;
+  align-items: center;
+  flex-shrink: 0;
+
+  @media (max-width: ${Breakpoint.sm}) {
+    flex-direction: column;
+    height: auto;
+    padding: 10px 0;
+  }
+`;
+
 const LinkWrapper = styled.span`
   margin: 0 0 0 10px;
+`;
+
+const LinksSection = styled.div``;
+
+const TextWrapper = styled.div`
+  margin: 0 10px 0 0;
+
+  @media (max-width: ${Breakpoint.sm}) {
+    margin: 0;
+  }
 `;
 
 const Footer = ({ text, links }: FooterProps): JSX.Element => {
   return (
     <Container>
-      <Text margin="0 10px 0 0">{text}</Text>
-      {links.map(
-        (link): JSX.Element => {
-          return (
-            <LinkWrapper key={link.id}>
-              <Link to={link.to}>{link.text}</Link>
-            </LinkWrapper>
-          );
-        }
-      )}
+      <TextWrapper>
+        <Text>{text}</Text>
+      </TextWrapper>
+      <LinksSection>
+        {links.map(
+          (link): JSX.Element => {
+            return (
+              <LinkWrapper key={link.id}>
+                <Link to={link.to}>{link.text}</Link>
+              </LinkWrapper>
+            );
+          }
+        )}
+      </LinksSection>
     </Container>
   );
 };
