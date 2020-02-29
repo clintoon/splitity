51e7c5d4cb4bad0b577cb9b7437af9eeafde6d40
clinton <clintonfeng@gmail.com>
2020-02-29 14:41:02 +1100

    implement auth controller

--- backend/app/controllers/auth_controller.rb
+++ backend/app/controllers/auth_controller.rb
@@ -1,2 +1,24 @@
+require 'rest-client'
+
 class AuthController < ApplicationController
+  def login
+    session_code = params[:code]
+
+    # TODO(clinton): Move to service class
+    response = RestClient.post('https://github.com/login/oauth/access_token',
+                               { client_id: Rails.application.credentials.github[:client_id],
+                                 client_secret: Rails.application.credentials.github[:client_secret],
+                                 code: session_code },
+                               accept: :json)
+    access_token = JSON.parse(response)['access_token']
+
+    session[:access_token] = access_token
+
+    head :no_content
+  end
+
+  def logout
+    session.clear
+    head :no_content
+  end
 end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  post 'v1/auth/login', to: 'auth#login'
+  post 'v1/auth/logout', to: 'auth#logout'
   post 'v1/repos/:owner/:repo_name/pulls/:pull_request_id/split', to: 'pull_requests#split'
   post 'v1/hooks/github', to: 'github_events#notify'
 end
