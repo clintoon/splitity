c3679bb37aa58f0216c2d56fcf5702a3429d7eb3
clinton <clintonfeng@gmail.com>
2020-02-29 16:52:01 +1100

    add comment

--- web/src/lib/eventHandlers/auth.ts
+++ web/src/lib/eventHandlers/auth.ts
@@ -4,7 +4,6 @@ import { v4 as uuidv4 } from 'uuid';
 import { githubConfig } from '@web/config/github';
 
 const handleSignIn = async (): Promise<void> => {
-  // TODO(clinton): handle signIn
   track(TrackingEvent.onRedirectToAuthScreen);
 
   const state = uuidv4();
