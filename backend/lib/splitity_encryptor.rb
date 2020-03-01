85dc2e77246c0915c36c09b3b0f5dab62ba6b0c9
clinton <clintonfeng@gmail.com>
2020-03-01 13:38:16 +1100

    change encryption service cipher to be aes128

--- backend/lib/splitity_encryptor.rb
+++ backend/lib/splitity_encryptor.rb
@@ -7,13 +7,13 @@ class SplitityEncryptor
 
   def encrypt(arg)
     serialized = arg.to_json
-    crypt = ActiveSupport::MessageEncryptor.new(@key)
+    crypt = ActiveSupport::MessageEncryptor.new(@key, cipher: 'aes-128-cbc')
     encrypted_data = crypt.encrypt_and_sign(serialized)
     encrypted_data
   end
 
   def decrypt(arg)
-    crypt = ActiveSupport::MessageEncryptor.new(@key)
+    crypt = ActiveSupport::MessageEncryptor.new(@key, cipher: 'aes-128-cbc')
     decrypted_data = crypt.decrypt_and_verify(arg)
     unserialized = JSON.parse(decrypted_data)
     unserialized
