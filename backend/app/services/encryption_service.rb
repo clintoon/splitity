c7023590ef9df276e8659c3c038085b94b0a4cab
clinton <clintonfeng@gmail.com>
2020-03-02 22:06:17 +1100

    use built in json serializer

--- backend/app/services/encryption_service.rb
+++ backend/app/services/encryption_service.rb
@@ -2,16 +2,14 @@ require 'json'
 
 class EncryptionService
   def self.encrypt_and_sign(payload)
-    crypt = ActiveSupport::MessageEncryptor.new(Rails.application.credentials.encryption_key, cipher: 'aes-128-cbc')
-    serialized = payload.to_json
-    encrypted_payload = crypt.encrypt_and_sign(serialized)
+    crypt = ActiveSupport::MessageEncryptor.new(Rails.application.credentials.encryption_key, cipher: 'aes-128-cbc', serializer: JSON)
+    encrypted_payload = crypt.encrypt_and_sign(payload)
     encrypted_payload
   end
 
   def self.decrypt_and_verify(encrypted_payload)
-    crypt = ActiveSupport::MessageEncryptor.new(Rails.application.credentials.encryption_key, cipher: 'aes-128-cbc')
-    serialized = crypt.decrypt_and_verify(encrypted_payload)
-    payload = JSON.parse(serialized)
+    crypt = ActiveSupport::MessageEncryptor.new(Rails.application.credentials.encryption_key, cipher: 'aes-128-cbc', serializer: JSON)
+    payload = crypt.decrypt_and_verify(encrypted_payload)
     payload
   end
 end
