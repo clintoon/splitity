7b70f0f84b3b264026ab94432afa0d7554cd956c
clinton <clintonfeng@gmail.com>
2020-03-02 20:43:10 +1100

    migrate to faraday

--- backend/app/controllers/auth_controller.rb
+++ backend/app/controllers/auth_controller.rb
@@ -1,4 +1,4 @@
-require 'rest-client'
+require 'faraday'
 require 'jwe'
 
 # TODO(clinton): Write unit tests
@@ -6,16 +6,18 @@ class AuthController < ApplicationController
   def login
     session_code = params[:code]
     # TODO(clinton): Move to service class and migrate to faraday
-    response = RestClient.post(
+    response = Faraday.post(
       'https://github.com/login/oauth/access_token',
       {
         client_id: Rails.application.credentials.github[:client_id],
         client_secret: Rails.application.credentials.github[:client_secret],
         code: session_code
       },
-      accept: :json
+      {
+        'Accept': 'application/json'
+      }
     )
-    access_token = JSON.parse(response)['access_token']
+    access_token = JSON.parse(response.body)['access_token']
 
     return head :internal_server_error if access_token.nil?
 
