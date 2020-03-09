6ec9a356959ea88d2e704528164234fd8dc8e566
Splitity <support@splitity.com>
2020-03-09 14:02:58 +0000

    Split pull request

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,4 +1,3 @@
-# TODO(clinton): write unit tests
 class User < ApplicationRecord
   validates :provider_user_id, uniqueness: true, presence: true
 end
--- backend/test/models/user_test.rb
+++ backend/test/models/user_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'invalid when provider_user_id is nil' do
+    user = build(:user)
+    user.provider_user_id = nil
+
+    assert user.invalid?
+  end
+
+  test 'invalid when provider_user_id is not unique' do
+    User.create(provider_user_id: 1)
+    user = build(:user)
+    user.provider_user_id = 1
+
+    assert user.invalid?
+  end
+
+  test 'valid when provider_user_id is unique' do
+    user = build(:user)
+    user.provider_user_id = 1
+
+    assert user.valid?
+  end
 end
