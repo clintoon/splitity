a729e7a7a80622f64a88df82e7a1b85f24a0a25c
Clinton Feng <clintonfeng@gmail.com>
2020-03-10 20:36:12 +1100

    Split pull request (#704)
    Co-authored-by: Splitity <support@splitity.com>

--- backend/test/controllers/current_user_controller_test.rb
+++ backend/test/controllers/current_user_controller_test.rb
@@ -1,7 +1,96 @@
 require 'test_helper'
 
 class CurrentUserControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'user_info returns unauthorized when auth header is invalid' do
+    Rails.application.credentials.stubs(:github).returns({ client_id: 'client_id', client_secret: 'client_secret' })
+
+    key = SecureRandom.random_bytes(16)
+    Rails.application.credentials.stubs(:encryption_key).returns(key)
+
+    GithubService.any_instance.stubs(:current_user).returns(nil)
+
+    get('/v1/current_user', { headers: { 'HTTP_AUTHORIZATION': 'Bearer abc123' } })
+
+    assert_response :unauthorized
+  end
+
+  test 'user_info returns unauthorized when gh access token is invalid' do
+    Rails.application.credentials.stubs(:github).returns({ client_id: 'client_id', client_secret: 'client_secret' })
+
+    key = SecureRandom.random_bytes(16)
+    Rails.application.credentials.stubs(:encryption_key).returns(key)
+
+    invalid_auth_token = EncryptionService.encrypt_and_sign('abc123', purpose: :login)
+
+    GithubService.any_instance.stubs(:current_user).returns(nil)
+
+    get('/v1/current_user', { headers: { 'HTTP_AUTHORIZATION': "Bearer #{invalid_auth_token}" } })
+
+    assert_response :unauthorized
+  end
+
+  test 'user_info returns success when auth token is valid' do
+    Rails.application.credentials.stubs(:github).returns({ client_id: 'client_id', client_secret: 'client_secret' })
+
+    key = SecureRandom.random_bytes(16)
+    Rails.application.credentials.stubs(:encryption_key).returns(key)
+
+    valid_auth_token = EncryptionService.encrypt_and_sign('abc123', purpose: :login)
+
+    GithubService.any_instance.stubs(:current_user).returns({ id: 123, login: 'clintoon' })
+
+    get('/v1/current_user', { headers: { 'HTTP_AUTHORIZATION': "Bearer #{valid_auth_token}" } })
+
+    parsed_resp = JSON.parse(@response.body)
+
+    assert_response :success
+    assert_equal parsed_resp['user_id'], 123
+  end
+
+  test 'github_app_installations returns unauthorized when auth header is invalid' do
+    Rails.application.credentials.stubs(:github).returns({ client_id: 'client_id', client_secret: 'client_secret' })
+
+    key = SecureRandom.random_bytes(16)
+    Rails.application.credentials.stubs(:encryption_key).returns(key)
+
+    GithubService.any_instance.stubs(:current_user).returns(nil)
+
+    get('/v1/current_user/github_app_installations', { headers: { 'HTTP_AUTHORIZATION': 'Bearer abc123' } })
+
+    assert_response :unauthorized
+  end
+
+  test 'github_app_installations returns unauthorized when gh access token is invalid' do
+    Rails.application.credentials.stubs(:github).returns({ client_id: 'client_id', client_secret: 'client_secret' })
+
+    key = SecureRandom.random_bytes(16)
+    Rails.application.credentials.stubs(:encryption_key).returns(key)
+
+    invalid_auth_token = EncryptionService.encrypt_and_sign('abc123', purpose: :login)
+
+    GithubService.any_instance.stubs(:current_user).returns(nil)
+
+    get('/v1/current_user/github_app_installations', { headers: { 'HTTP_AUTHORIZATION': "Bearer #{invalid_auth_token}" } })
+
+    assert_response :unauthorized
+  end
+
+  test 'github_app_installations returns success when auth token is valid' do
+    Rails.application.credentials.stubs(:github).returns({ client_id: 'client_id', client_secret: 'client_secret' })
+
+    key = SecureRandom.random_bytes(16)
+    Rails.application.credentials.stubs(:encryption_key).returns(key)
+
+    valid_auth_token = EncryptionService.encrypt_and_sign('abc123', purpose: :login)
+
+    GithubService.any_instance.stubs(:current_user).returns({ id: 123, login: 'clintoon' })
+    GithubService.any_instance.stubs(:app_installations_for_user).returns([{ installation_id: 1, account_id: 1 }, { installation_id: 2, account_id: 2 }])
+
+    get('/v1/current_user/github_app_installations', { headers: { 'HTTP_AUTHORIZATION': "Bearer #{valid_auth_token}" } })
+
+    parsed_resp = JSON.parse(@response.body)
+
+    assert_response :success
+    assert_equal parsed_resp['installations'], [{ 'installation_id' => 1, 'account_id' => 1 }, { 'installation_id' => 2, 'account_id' => 2 }]
+  end
 end
