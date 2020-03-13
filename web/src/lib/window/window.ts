146ed6840ea95f4a75509d9e2832b9c2be0d7837
Clinton Feng <clintonfeng@gmail.com>
2020-03-13 21:32:36 +1100

    Fix auth callback on github app install (#711)

--- web/src/lib/window/window.ts
+++ web/src/lib/window/window.ts
@@ -6,8 +6,22 @@ const getSessionStorageItem = (key: string): string | null => {
   return window.sessionStorage.getItem(key);
 };
 
+const removeSessionStorageItem = (key: string): void => {
+  window.sessionStorage.removeItem(key);
+};
+
 const setHref = (url: string): void => {
   window.location.href = url;
 };
 
-export { setSessionStorageItem, getSessionStorageItem, setHref };
+const clearQueryParams = (): void => {
+  window.history.replaceState(null, document.title, window.location.pathname);
+};
+
+export {
+  setSessionStorageItem,
+  getSessionStorageItem,
+  removeSessionStorageItem,
+  setHref,
+  clearQueryParams,
+};
