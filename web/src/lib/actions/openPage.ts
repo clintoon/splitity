d7029050eb833b829e1ae1f1d109baa6eb31e97a
clinton <clintonfeng@gmail.com>
2020-03-13 21:42:43 +1100

    redirect instead of new tab

--- web/src/lib/actions/openPage.ts
+++ web/src/lib/actions/openPage.ts
@@ -1,17 +1,16 @@
 import { githubConfig } from '@web/config/github';
 import { track } from '../analytics/tracking';
 import { TrackingEvent } from '../analytics/events';
+import { setHref } from '../window/window';
 
 const onAddReposClick = (githubInstallationId: number): void => {
   track(TrackingEvent.openConfigureAppInstallationPage);
-  window.open(
-    `https://github.com/settings/installations/${githubInstallationId}`
-  );
+  setHref(`https://github.com/settings/installations/${githubInstallationId}`);
 };
 
 const onInstallGithubApp = (): void => {
   track(TrackingEvent.openInstallAppPage);
-  window.open(`https://github.com/apps/${githubConfig.githubAppName}`);
+  setHref(`https://github.com/apps/${githubConfig.githubAppName}`);
 };
 
 export { onAddReposClick, onInstallGithubApp };
--- web/src/lib/actions/openPage.unit.test.ts
+++ web/src/lib/actions/openPage.unit.test.ts
@@ -2,21 +2,19 @@ import { onAddReposClick, onInstallGithubApp } from '@web/lib/actions/openPage';
 import { PROCESS_ENV_GITHUB_APP_NAME } from '@web/testing/testGlobals';
 import { track } from '@web/lib/analytics/tracking';
 import { TrackingEvent } from '../analytics/events';
+import { setHref } from '@web/lib/window/window';
 
 const GITHUB_INSTALLATION_ID = 123;
 
 jest.mock('@web/lib/analytics/tracking');
+jest.mock('@web/lib/window/window');
 
 describe('openPage', (): void => {
   describe('onAddReposClick', (): void => {
     it('opens new tab with correct url', (): void => {
-      const windowOpenSpy = jest
-        .spyOn(window, 'open')
-        .mockImplementation((): null => null);
-
       onAddReposClick(GITHUB_INSTALLATION_ID);
 
-      expect(windowOpenSpy).toBeCalledWith(
+      expect(setHref).toBeCalledWith(
         `https://github.com/settings/installations/${GITHUB_INSTALLATION_ID}`
       );
     });
@@ -34,21 +32,15 @@ describe('openPage', (): void => {
 
   describe('onInstallGithubApp', (): void => {
     it('opens new tab with correct url', (): void => {
-      const windowOpenSpy = jest
-        .spyOn(window, 'open')
-        .mockImplementation((): null => null);
-
       onInstallGithubApp();
 
-      expect(windowOpenSpy).toBeCalledWith(
+      expect(setHref).toBeCalledWith(
         `https://github.com/apps/${PROCESS_ENV_GITHUB_APP_NAME}`
       );
     });
   });
 
   it('calls the openConfigureAppInstallationPage tracking event', (): void => {
-    jest.spyOn(window, 'open').mockImplementation((): null => null);
-
     onInstallGithubApp();
 
     expect(track).toBeCalledWith(TrackingEvent.openInstallAppPage);
